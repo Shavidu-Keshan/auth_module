4bef1037d8806726831119d8fcf9ede99e26c2cb
Shavidu Keshan <jkskeshan@sat.appsc.sab.ac.lk>
2024-05-05 09:43:02 +0530

    create simple authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'profile_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyCihasdJR9fY-qPSeqXv5UplrC89H1CGIE',
+          appId: '1:209296452477:android:afc819d39e90d427c73a14',
+          messagingSenderId: '209296452477',
+          projectId: 'flutter-login-180e2'));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); // Fix: Added Key? key parameter
+  const MyApp({super.key}); // Fix: Added Key? key parameter
   @override // Fix: Added @override annotation
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomePage(),
     );
   }
@@ -18,6 +30,10 @@ class MyApp extends StatelessWidget {
     Step 1: create the main layout of the app( only the UI)
     Step 2:login to your firebase console
     step 3:create new firebase project
+    step 4: let,s add the firebase dependencisis
+    step 5:let,s init's the fire base project
+    step 6:create the login function
+    step 7
 
  */
 
@@ -25,35 +41,75 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  //Initialize Firebase app
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: LoginScreen());
+    return Scaffold(
+        body: FutureBuilder(
+            future: _initializeFirebase(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return LoginScreen();
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }));
   }
 }
 
+@override
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //login function
+  static Future<User?> loginUsingEmailPassword(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        print("No User found for that email");
+      }
+    }
+
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
+    //create the textfield controller
+    TextEditingController _emailController = TextEditingController();
+    TextEditingController _passwordcontroller = TextEditingController();
     return Padding(
       padding:
           const EdgeInsets.all(16.0), // Changed to a single value for all sides
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           const Text(
-            "MyApp Title",
+            "FoodMarket",
             style: TextStyle(
               color: Colors.black,
               fontSize: 28.0,
@@ -61,7 +117,7 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
           ),
           const Text(
-            "Login to your App",
+            "Login to FoodMarket",
             style: TextStyle(
                 color: Colors.black,
                 fontSize: 44.0,
@@ -70,9 +126,10 @@ class _LoginScreenState extends State<LoginScreen> {
           const SizedBox(
             height: 44.0,
           ),
-          const TextField(
+          TextField(
+            controller: _emailController,
             keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               hintText: "User Email",
               prefixIcon: Icon(Icons.mail, color: Colors.black),
             ),
@@ -80,9 +137,10 @@ class _LoginScreenState extends State<LoginScreen> {
           const SizedBox(
             height: 26.0,
           ),
-          const TextField(
+          TextField(
+            controller: _passwordcontroller,
             obscureText: true,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               hintText: "User Password",
               prefixIcon: Icon(Icons.lock, color: Colors.black),
             ),
@@ -100,17 +158,31 @@ class _LoginScreenState extends State<LoginScreen> {
           Container(
             width: double.infinity,
             child: RawMaterialButton(
-                fillColor: Color(0xFF0069FE),
+                fillColor: const Color(0xFF0069FE),
                 elevation: 0.0,
-                padding: EdgeInsets.symmetric(vertical: 20.0),
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(12.0)),
-                onPressed: () {},
-                child: Text(
+                onPressed: () async {
+                  //lets the app
+                  // ignore: non_constant_identifier_names
+                  User? user = await loginUsingEmailPassword(
+                      email: _emailController.text,
+                      password: _passwordcontroller.text,
+                      context: context);
+                  print(user);
+                  if (user != null) {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => ProfileScreen()));
+                    //lets make a new screen
+                  }
+                },
+                child: const Text(
                   "login",
                   style: TextStyle(color: Colors.white, fontSize: 18.0),
                 )),
           )
+
           // Add your widgets here
         ],
       ),
